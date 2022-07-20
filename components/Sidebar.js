b071a8353870cb566aa2f5c6b7f4d86b4c17ecb9
Budi I. Gunawan <budiindra.g@gmail.com>
2022-07-20 23:44:36 +0700

    create active link and change bg color

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,95 @@
 import React from 'react';
+import { Box, Icon, useColorModeValue, Text } from '@chakra-ui/react';
+import { FiTrendingUp, FiCompass, FiStar } from 'react-icons/fi';
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import { GiWorld } from 'react-icons/gi';
+import { AiFillInfoCircle } from 'react-icons/ai';
+
+const LinkItems = [
+  { name: 'Browse', url: '/', icon: GiWorld },
+  { name: 'Suggest Attraction', url: '/trending', icon: FiTrendingUp },
+  { name: 'Videos', url: '/explore', icon: FiCompass },
+  { name: 'Blog', url: '/favourites', icon: FiStar },
+  { name: 'About', url: '/settings', icon: AiFillInfoCircle },
+];
+
+export default function SimpleSidebar({ children }) {
+  return (
+    <Box minH='100vh' bg={useColorModeValue('gray.100', 'gray.900')}>
+      <SidebarContent
+        onClose={() => onClose}
+        display={{ base: 'none', md: 'block' }}
+      />
+      <Box ml={{ base: 0, md: 60 }} p='4'>
+        {children}
+      </Box>
+    </Box>
+  );
+}
+
+const SidebarContent = () => {
+  const router = useRouter();
+
+  return (
+    <Box
+      bg={'#313541'}
+      borderRight='1px'
+      borderRightColor={useColorModeValue('gray.200', 'gray.700')}
+      w={{ base: 'full', md: '150px' }}
+      pos='fixed'
+      h='full'
+    >
+      {LinkItems.map((link) => (
+        <NavItem
+          key={link.name}
+          icon={link.icon}
+          url={link.url}
+          isActive={router.pathname === link.url}
+        >
+          {link.name}
+        </NavItem>
+      ))}
+    </Box>
+  );
 };
 
-export default Sidebar;
+const NavItem = ({ icon, url, isActive, children }) => {
+  return (
+    <Link href={url}>
+      <Box
+        minH={'124px'}
+        role='group'
+        cursor='pointer'
+        bg={isActive && '#72cdd2'}
+        color={isActive ? 'white' : '#8298A0'}
+        _hover={{
+          bg: '#72cdd2',
+          color: 'white',
+        }}
+        position='relative'
+      >
+        <Box
+          width='80px'
+          height='80px'
+          position='absolute'
+          top='50%'
+          left='50%'
+          textAlign='center'
+          margin='-40px 0 0 -40px'
+        >
+          <Icon
+            fontSize='44'
+            _groupHover={{
+              color: 'white',
+            }}
+            as={icon}
+          />
+          <Text>{children}</Text>
+        </Box>
+      </Box>
+    </Link>
+  );
+};
